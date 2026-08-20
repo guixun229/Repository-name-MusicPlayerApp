52507d57964bec0a781ae4feef53cd956939673f
guixun229 <1053171542@qq.com>
2026-08-20 09:52:41 +0800

    Add files via upload

---
+++ 6a85177c415bc3591d389f5f_YinguMusicPlugin.java
@@ -0,0 +1,165 @@
+package com.yingu.player;
+
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.util.Log;
+import android.webkit.WebView;
+
+/**
+ * 音轨 Cordova 插件
+ * 桥接 Web 与 Native 音乐服务
+ */
+public class YinguMusicPlugin extends CordovaPlugin {
+
+    private static final String TAG = "YinguMusicPlugin";
+    private static CallbackContext controlCallback = null;
+    private static YinguMusicPlugin instance = null;
+
+    // 广播接收器 - 接收 MusicService 发来的控制指令
+    private final BroadcastReceiver serviceControlReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.hasExtra("control")) {
+                String control = intent.getStringExtra("control");
+                Log.d(TAG, "收到服务控制指令: " + control);
+                sendControlToJs(control);
+            }
+        }
+    };
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        instance = this;
+        Log.d(TAG, "音轨插件初始化");
+
+        // 注册广播接收器
+        IntentFilter filter = new IntentFilter("com.yingu.player.CONTROL");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            cordova.getActivity().registerReceiver(serviceControlReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            cordova.getActivity().registerReceiver(serviceControlReceiver, filter);
+        }
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        Log.d(TAG, "execute: " + action);
+
+        switch (action) {
+            case "updatePlayback": {
+                boolean isPlaying = args.getBoolean(0);
+                String trackName = args.getString(1);
+                int trackIndex = args.getInt(2);
+                int trackTotal = args.getInt(3);
+
+                updatePlaybackState(isPlaying, trackName, trackIndex, trackTotal);
+                callbackContext.success();
+                return true;
+            }
+
+            case "startService": {
+                startMusicService();
+                callbackContext.success();
+                return true;
+            }
+
+            case "stopService": {
+                stopMusicService();
+                callbackContext.success();
+                return true;
+            }
+
+            case "registerControl": {
+                // 保持回调，后续通过此回调发送控制指令
+                PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+                result.setKeepCallback(true);
+                callbackContext.sendPluginResult(result);
+                controlCallback = callbackContext;
+                return true;
+            }
+
+            default:
+                return false;
+        }
+    }
+
+    private void updatePlaybackState(boolean isPlaying, String trackName, int trackIndex, int trackTotal) {
+        Context context = cordova.getActivity();
+        Intent intent = new Intent(context, MusicService.class);
+        intent.setAction(MusicService.ACTION_UPDATE);
+        intent.putExtra(MusicService.EXTRA_IS_PLAYING, isPlaying);
+        intent.putExtra(MusicService.EXTRA_TRACK_NAME, trackName);
+        intent.putExtra(MusicService.EXTRA_TRACK_INDEX, trackIndex);
+        intent.putExtra(MusicService.EXTRA_TRACK_TOTAL, trackTotal);
+
+        if (isPlaying) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(intent);
+            } else {
+                context.startService(intent);
+            }
+        } else {
+            context.startService(intent);
+        }
+    }
+
+    private void startMusicService() {
+        Context context = cordova.getActivity();
+        Intent intent = new Intent(context, MusicService.class);
+        intent.setAction(MusicService.ACTION_START);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent);
+        } else {
+            context.startService(intent);
+        }
+    }
+
+    private void stopMusicService() {
+        Context context = cordova.getActivity();
+        Intent intent = new Intent(context, MusicService.class);
+        intent.setAction(MusicService.ACTION_STOP);
+        context.startService(intent);
+    }
+
+    /**
+     * 将控制指令发送到 JavaScript 层
+     */
+    private void sendControlToJs(final String control) {
+        cordova.getActivity().runOnUiThread(() -> {
+            // 方法1: 通过 PluginResult 发送 (如果回调还活着)
+            if (controlCallback != null) {
+                PluginResult result = new PluginResult(PluginResult.Status.OK, control);
+                result.setKeepCallback(true);
+                controlCallback.sendPluginResult(result);
+            }
+
+            // 方法2: 同时通过 loadUrl 调用 (备用)
+            String js = "javascript:if(window.YinguMusic && window.YinguMusic._onControl) window.YinguMusic._onControl('" + control + "');";
+            webView.loadUrl(js);
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        stopMusicService();
+        try {
+            cordova.getActivity().unregisterReceiver(serviceControlReceiver);
+        } catch (Exception e) {
+            Log.w(TAG, "Receiver not registered", e);
+        }
+        instance = null;
+        super.onDestroy();
+    }
+}
